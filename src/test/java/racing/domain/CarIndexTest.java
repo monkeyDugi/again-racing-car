235ee8f946df7e9cca847848cb93c3eaa95d8729
monkeyDugi <lbd4946@gmail.com>
2021-12-04 15:45:38 +0900

    style : static 메서드 import 처리

--- src/test/java/racing/domain/CarIndexTest.java
+++ src/test/java/racing/domain/CarIndexTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CarIndexTest {
@@ -36,7 +37,7 @@ class CarIndexTest {
     @Test
     void exception_minimum_index() {
         // when
-        Assertions.assertThatThrownBy(() -> new CarIndex(-1))
+        assertThatThrownBy(() -> new CarIndex(-1))
                 // then
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("최소 시작 index는 0이상 이어야 합니다.");
@@ -46,7 +47,7 @@ class CarIndexTest {
     @Test
     void exception_not_move_index() {
         // when
-        Assertions.assertThatThrownBy(() -> new CarIndex().getLastIndex())
+        assertThatThrownBy(() -> new CarIndex().getLastIndex())
                 // then
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("자동차가 이동한 기록이 없습니다.");
